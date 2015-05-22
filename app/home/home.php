516fee9e0d3fa16429a6f58fe1b1dde7c9933b55
YerkoPalma <yerko.palma@usach.cl>
2015-05-21 23:43:27 -0300

    added basic support for filters and functions

--- app/home/home.php
+++ app/home/home.php
@@ -2,10 +2,21 @@
 class Home extends Page
 {
 	public $name = "Home";
+	public $filters = array('lower_case', 'not_a_filter');
+	public $functions = array();
 
 	function __construct(){
 		parent::__construct($this->name);
-		$this->set("test", "primer test");
+		$this->set("test", "PriMer Test");
+		$this->set("go", "SEGUNDO");
+	}
+
+	function lower_case($txt){
+		return strtolower($txt);
+	}
+
+	function not_a_filter($txt){
+		return strtoupper($txt);
 	}
 	
 }
--- index.php
+++ index.php
@@ -11,6 +11,41 @@
 
 	$home = new Home();
 
+	#obtener nombres de metodos
+	$homeClass = get_class($home);
+	$homeMethods = get_class_methods($homeClass);
+
+	$homeVars = get_object_vars($home);
+
+	#obtener nombred de filtros
+	$homeFilters = $homeVars['filters'];
+
+	#obtener nombre de funciones
+	$homeFunctions = $homeVars['functions'];
+
+	#por cada metodo
+	foreach ($homeMethods as $homeMethod) {
+
+		#si es un filtro, agregar como filtro de twig
+		if(in_array($homeMethod, $homeFilters)){
+			#Registro el filtro
+			$filter = new Twig_SimpleFilter($homeMethod, array($homeClass, $homeMethod));
+
+			#Lo agrego al template
+			$twig->addFilter($filter);
+		}
+
+		#si es una funcion, agregar como funcion de twig
+		if(in_array($homeMethod, $homeFunctions)){
+			#Registro el filtro
+			$function = new Twig_SimpleFunction($homeMethod, array($homeClass, $homeMethod));
+
+			#Lo agrego al template
+			$twig->addFunction($function);
+		}
+	}
+		
+
 	echo $twig->render($home->templateDir(), $home->vars)
 
 ?>
